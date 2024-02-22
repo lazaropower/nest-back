1677c6d3357fa588750afe7126af8706eae6bfb4
Lázaro <20268108+lazaropower@users.noreply.github.com>
2024-02-22 16:54:01 +0200

    Add auth and users module. Add signIn path and mocked users

---
+++ src/users/users.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+
+// This should be a real class/interface representing a user entity
+export type User = any;
+
+@Injectable()
+export class UsersService {
+    private readonly users = [
+      {
+        userId: 1,
+        email: 'hola@hola.es',
+        password: '123',
+      },
+      {
+        userId: 2,
+        email: 'adios@adios.es',
+        password: 'password1',
+      },
+    ];
+  
+    async findOne(email: string): Promise<User | undefined> {
+      return this.users.find(user => user.email === email);
+    }
+}
