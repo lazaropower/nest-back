b2f1ab0347f16b4fe2b059d81e0f3b5a34104e3d
Lázaro <20268108+lazaropower@users.noreply.github.com>
2024-02-27 14:15:25 +0200

    Add and configure helmet and cors

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,6 +22,9 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
+
+  app.use(helmet());
+  app.enableCors();
 
   await app.listen(3333);
 }
